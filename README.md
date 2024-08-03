666aa0049cd5e5eba1243414c0024540e393b480
Adriano Jr <adrianojbarros5@gmail.com>
2024-08-03 13:58:43 -0300

    pagina de desafios

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # adrianojr.online
+
+WEBSITE PESSOAL - INICIO V2 02/08/2024
+
+Template figma: https://www.figma.com/design/x3luGI7hf2wB5edO0eOvS9/Novo-Design---Site?m=auto&t=W4t317LJAE8HIA3L-6
---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const email = "teste@teste.teste"
+
+function irInicio(){
+    let botoes = document.getElementById('funcao_selecionar_linguagens');
+    botoes.style.display = 'flex';
+    let sobre = document.getElementById('texto_paragrafo_conteudo');
+    sobre.innerHTML = `Esse site tem como funçao, guardar alguns testes feitos com diferentes linguagens, para que possa ser usada por outras pessoas futuramente, ou poder auxiliair os novos programadores`;
+}
+
+function irSobre(){
+    let botoes = document.getElementById('funcao_selecionar_linguagens');
+    botoes.style.display = 'none';
+    
+    let sobre = document.getElementById('texto_paragrafo_conteudo');
+    sobre.innerHTML = `Embora este seja um site pessoal onde exploro novas tecnologias, meu objetivo é oferecer algo valioso tanto para programadores iniciantes quanto para aqueles que já estão em atividade. Em razão disso, adiciono desafios que crio pessoalmente, reconhecendo que o processo de ensinar é crucial para a fixação dos conhecimentos recém-adquiridos. Além de servir como um recurso para novos programadores, esta página também visa fornecer orientações e desafios que são essenciais para o desenvolvimento contínuo desses profissionais. Os desafios são cuidadosamente elaborados e adicionados manualmente, um por um, para assegurar uma curadoria adequada e relevante. Se você desejar colaborar com novos desafios, sinta-se à vontade para enviá-los para: ${email}`;
+}
