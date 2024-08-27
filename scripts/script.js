41182ac4a0bb93f15fef8abf3e86cf37d0b2291e
Adriano Jr <adrianojunr@gmail.com>
2024-08-27 13:04:18 -0300

    Criação icones, pagina politica privacidade, styles

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,15 @@
-const email = "teste@teste.teste"
+const email = "cont@adrianojr.online"
 
 function irInicio(){
-    let botoes = document.getElementById('funcao_selecionar_linguagens');
-    botoes.style.display = 'block';
+        let botoes = document.getElementById('funcao_selecionar_linguagens');
+
+    if (window.innerWidth <= 1000) { // Considerando 768px como limite para mobile
+        botoes.style.display = 'block'; // Para mobile
+    } else {
+        botoes.style.display = 'flex'; // Para PC
+    }
     let sobre = document.getElementById('texto_paragrafo_conteudo');
-    sobre.innerHTML = `Esse site tem como funçao, guardar alguns testes feitos com diferentes linguagens, para que possa ser usada por outras pessoas futuramente, ou poder auxiliair os novos programadores`;
+    sobre.innerHTML = `Este site tem como finalidade armazenar uma variedade de testes realizados em diferentes linguagens de programação, visando proporcionar um recurso valioso para futuros usuários. Além disso, busca-se oferecer suporte e orientação a novos programadores, facilitando seu aprendizado e desenvolvimento.`;
 }
 
 function irSobre(){
@@ -12,7 +17,8 @@ function irSobre(){
     botoes.style.display = 'none';
     
     let sobre = document.getElementById('texto_paragrafo_conteudo');
-    sobre.innerHTML = `Embora este seja um site pessoal onde exploro novas tecnologias, meu objetivo é oferecer algo valioso tanto para programadores iniciantes quanto para aqueles que já estão em atividade. Em razão disso, adiciono desafios que crio pessoalmente, reconhecendo que o processo de ensinar é crucial para a fixação dos conhecimentos recém-adquiridos. Além de servir como um recurso para novos programadores, esta página também visa fornecer orientações e desafios que são essenciais para o desenvolvimento contínuo desses profissionais. Os desafios são cuidadosamente elaborados e adicionados manualmente, um por um, para assegurar uma curadoria adequada e relevante. Se você desejar colaborar com novos desafios, sinta-se à vontade para enviá-los para: ${email}`;
+    sobre.innerHTML = `Embora este seja um site pessoal onde exploro novas tecnologias, meu objetivo é oferecer algo valioso tanto para programadores iniciantes quanto para aqueles que já estão em atividade. Em razão disso, adiciono desafios que crio pessoalmente, reconhecendo que o processo de ensinar é crucial para a fixação dos conhecimentos recém-adquiridos. Além de servir como um recurso para novos programadores, esta página também visa fornecer orientações e desafios que são essenciais para o desenvolvimento contínuo desses profissionais. Os desafios são cuidadosamente elaborados e adicionados manualmente, um por um, para assegurar uma curadoria adequada e relevante. Se você desejar colaborar com novos desafios ou ideias para implementar, sinta-se à vontade para enviá-los para: ${email}`;
+    sobre.style.margin = "8.4rem";
 }
 
 function redirecionarPgithubTopic(){
